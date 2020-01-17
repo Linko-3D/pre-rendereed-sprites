722337b5a1e66ce474c7bf685639ba3ed085d470
Linko-3D <dbekhouc@gmail.com>
2020-01-17 16:14:58 +0100

    tweak

--- Level.gd
+++ Level.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
 var zoom = 0.3
+var pan = false
 
-func _process(delta):
-	if Input.is_mouse_button_pressed(BUTTON_WHEEL_DOWN):
-		print("a")
-	if Input.is_action_pressed("ui_down"):
-		zoom += 0.005
-		$Camera2D.zoom = Vector2(zoom, zoom)
-	if Input.is_action_pressed("ui_up"):
-		zoom -= 0.005
-		$Camera2D.zoom = Vector2(zoom, zoom)
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_WHEEL_DOWN and event.pressed:
+			if $Camera2D.zoom.x < 0.9:
+				zoom += 0.05
+				$Camera2D.zoom = Vector2(zoom, zoom)
+		if event.button_index == BUTTON_WHEEL_UP:
+			if $Camera2D.zoom.x > 0.3:
+				zoom -= 0.05
+				$Camera2D.zoom = Vector2(zoom, zoom)
+		
+		if event.button_index == BUTTON_MIDDLE and event.pressed:
+			pan = true
+		if event.button_index == BUTTON_MIDDLE and event.pressed == false:
+			pan = false
+		
+	if event is InputEventMouseMotion:
+		if pan:
+			$Camera2D.position -= event.relative / 2
